732984d26c2d941c8ab09ae2335fe5e609588ca2
belonel <ksandr.lr@gmail.com>
2020-01-04 19:40:41 +0300

    Greeting page completed. Mapper with a name variable. It's printing "Hello, " + name. Using thymeleaf as template, Maven and SpringBoot.

---
+++ src/main/java/com/lider/BlockNoteWebApp/Application.java
@@ -0,0 +1,13 @@
+package com.lider.BlockNoteWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
